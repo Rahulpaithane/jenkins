5c7ba84d4bfc637f8957217ad5e87ffb7ca764d4
Rahul <rahulpaithane25@gmail.com>
2022-02-01 23:33:32 -0500

    Added Workspace Path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,5 @@ echo "*******************************"
 echo "********Building Jar **********"
 echo "*******************************"
 
-docker run -ti --rm  -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE = "/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-new"
+docker run -ti --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
