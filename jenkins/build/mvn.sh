e1ae77687fffa908ee3dad0654fe7c849dca77ec
Rahul Paithane <rahulpaithane25@gmail.com>
2022-02-02 10:31:25 +0530

    syntax error fixed

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "*******************************"
 echo "********Building Jar **********"
 echo "*******************************"
 
-WORKSPACE = "/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-new"
+WORKSPACE="/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-new"
 docker run -ti --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
