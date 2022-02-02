67bcab3093756b9cafdddf1b65d3f52a87ee8884
Rahul Paithane <rahulpaithane25@gmail.com>
2022-02-02 10:33:42 +0530

    Bug fixed

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,5 +5,5 @@ echo "********Building Jar **********"
 echo "*******************************"
 
 WORKSPACE="/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-new"
-docker run -ti --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,4 +6,4 @@ echo "*******************************"
 
 WORKSPACE="/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-new"
 
-docker run -ti --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
